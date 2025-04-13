e0efa21bd907a72b10f1750d2e0a0a57bc265e53
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-13 17:25:02 +0200

    padding

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import About from '../About/About'
 export default function Home() {
     return (
         <>
-            <section className='flex md:flex-row flex-col-reverse items-center  px-10 md:px-[120px] pb-16 bg-mowana-light gap-14 pt-36' >
+            <section className='flex md:flex-row flex-col-reverse items-center h-screen px-10 md:px-[120px] pb-16 bg-mowana-light gap-14 md:pt-36' >
                 <div className='w-full md:w-4/12 flex flex-col gap-5 md:gap-8'>
                     <h2 className='font-[quando] font-bold text-mowana md:text-4xl text-2xl'> <span className=' text-4xl lg:text-6xl'>Mowana</span> Where Luxury Meets..</h2>
                     <p className='font-[poppins] lg:text-3xl text-2xl'>Tranquility in the heart of breathtaking nature..</p>
