52460f6e68c58c3cdbd107a1605f24319da2807f
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-14 19:04:21 +0200

    footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { easeIn, motion } from "framer-motion";
-import img from "../../assets/images/logo1.png";
+import thank from "../../assets/images/thank.png";
 
 
 
@@ -17,7 +17,7 @@ export default function Contact() {
             transition={{ duration: .5 }}
             className="text-center font-bold font-[quando] uppercase text-mowana  text-4xl mb-8">contact us</motion.h2>
 
-        <section className='mx-auto px-12 md:px-[120px] flex  flex-col lg:flex-row items-center gap-y-4  justify-between overflow-hidden py-8 bg-nav'>
+        <section className='mx-auto px-12 md:px-[120px] flex  flex-col lg:flex-row items-center gap-y-9  justify-between overflow-hidden py-8 bg-nav'>
 
 
 
@@ -52,7 +52,7 @@ export default function Contact() {
                     initial={{ x: -200, opacity: 0 }}
                     whileInView={{ x: 0, opacity: 1 }}
                     transition={{ duration: .5, ease: "easeIn" }}
-                    src={img} alt="" />
+                    src={thank} alt="" />
             </div>
 
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import img from "../../assets/images/logo1.png";
+
+
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <>
+
+      <section className='font-[poppins] mx-auto px-12 md:px-[120px] py-10 overflow-hidden bg-gradient-to-t from-[#F2FF4D] to-[#F8FEAB] flex flex-col lg:flex-row justify-between items-center'>
+
+
+        <div className='text-mowana font-semibold text-2xl'>
+          <p>Phone Support</p>
+          <span>+20 123 456 789</span>
+        </div>
+        <div>
+          <img src={img} className='w-[300px]' alt="" />
+        </div>
+        <div className=''>
+          <p className='text-center mb-3 text-2xl font-[poppins] font-semibold'>Follow Us</p>
+          <div className='flex items-center gap-7 justify-center text-2xl '>
+            <i className='fa-brands cursor-pointer fa-facebook-f'></i>
+            <i className='fa-brands cursor-pointer fa-twitter'></i>
+            <i className='fa-brands cursor-pointer fa-youtube'></i>
+            <i className='fa-brands cursor-pointer fa-instagram'></i>
+          </div>
+        </div>
+
+
+      </section>
+      <div className='bg-green-900 w-full h-16 px-1 text-[#F2FF4D] text-center flex justify-center items-center'>
+        © Copyright 2017 Mowana hotel - All Rights Reserved
+      </div>
+
+    </>
   )
 }
